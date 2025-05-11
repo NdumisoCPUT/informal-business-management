0868059bf13ad9d0a5f025c8f9ad00458ede793d
NdumisoCPUT <220094861@mycput.ac.za>
2025-05-11 14:51:59 +0200

    Fix InMemoryOrderRepository methods and InventoryItem structure for CI

--- repositories/inmemory/order_repository.py
+++ repositories/inmemory/order_repository.py
@@ -4,15 +4,15 @@ class InMemoryOrderRepository:
     def __init__(self):
         self._storage = {}
 
-    def add_order(self, order: Order):
+    def save(self, order: Order):
         self._storage[order.get_order_id()] = order
         return order
 
-    def get_order(self, order_id: str):
+    def find_by_id(self, order_id: str):
         return self._storage.get(order_id)
 
-    def list_orders(self):
+    def list_all(self):
         return list(self._storage.values())
 
-    def remove_order(self, order_id: str):
+    def delete(self, order_id: str):
         return self._storage.pop(order_id, None)
